c2b7cc39697db16a5273305b3338e46fc670cf76
Will Mendes <willmendesneto@gmail.com>
2021-10-29 11:57:56 -0300

    chore: adding hook for pre-commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 ### Added
 
 - Adding `ES2020` bundle to be published
+- Adding pre-commit hook
 
 ### Fixed
 
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-// eslint-disable-next-line no-undef
 module.exports = {
   testEnvironment: 'jsdom',
   // The directory where Jest should store its cached dependency information
